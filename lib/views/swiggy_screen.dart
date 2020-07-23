a7a0d7ed451444bc55e6b8f229ae88a391c83809
Vinoth Vino <vinothvino42@gmail.com>
2020-07-23 08:00:28 +0530

    Add top picks and banner view

--- lib/views/swiggy_screen.dart
+++ lib/views/swiggy_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:swiggy_ui/utils/app_colors.dart';
 import 'package:swiggy_ui/utils/ui_helper.dart';
+import 'package:swiggy_ui/widgets/custom_divider_view.dart';
 
 class SwiggyScreen extends StatelessWidget {
   @override
@@ -8,11 +10,22 @@ class SwiggyScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 15.0),
           child: Column(
             children: <Widget>[
               _buildAppBar(context),
-              _FoodGroceriesAvailabilityView()
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      _FoodGroceriesAvailabilityView(),
+                      _TopPicksForYouView(),
+                      _OfferBannerView(),
+                      CustomDividerView(),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -21,6 +34,7 @@ class SwiggyScreen extends StatelessWidget {
   }
 
   Container _buildAppBar(BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15.0),
         height: 60.0,
         child: Row(
           children: <Widget>[
@@ -55,6 +69,7 @@ class _FoodGroceriesAvailabilityView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
       child: Column(
         children: <Widget>[
           Row(
@@ -186,11 +201,10 @@ class _FoodGroceriesAvailabilityView extends StatelessWidget {
         child: Column(
           children: <Widget>[
             ClipRRect(
-              borderRadius: BorderRadius.circular(12.0),
+              borderRadius: BorderRadius.circular(16.0),
               child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
                 padding: const EdgeInsets.only(top: 8.0),
-                height: 120.0,
                 decoration:
                     BoxDecoration(color: swiggyOrange, boxShadow: <BoxShadow>[
                   BoxShadow(
@@ -201,6 +215,7 @@ class _FoodGroceriesAvailabilityView extends StatelessWidget {
                 ]),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     Text(
                       'Genie',
@@ -225,4 +240,130 @@ class _FoodGroceriesAvailabilityView extends StatelessWidget {
           ],
         ),
       );
+}
+
+class _TopPicksForYouView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(Icons.thumb_up, size: 20.0),
+              UIHelper.horizontalSpaceSmall(),
+              Text(
+                'Top Picks For You',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    .copyWith(fontSize: 20.0),
+              )
+            ],
+          ),
+          UIHelper.verticalSpaceLarge(),
+          Container(
+            height: 165.0,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) => Container(
+                margin: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 2.0,
+                            )
+                          ]),
+                      child: Image.asset(
+                        'assets/images/food2.jpg',
+                        width: 100.0,
+                        height: 100.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    UIHelper.verticalSpaceSmall(),
+                    Text(
+                      'Hotel Chennai',
+                      style: Theme.of(context).textTheme.subtitle2.copyWith(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w700,
+                          ),
+                    ),
+                    UIHelper.verticalSpaceExtraSmall(),
+                    Text(
+                      '34 min',
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            color: Colors.grey[700],
+                            fontSize: 12.0,
+                          ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _OfferBannerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 15.0),
+      height: 180.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Swiper(
+        itemHeight: 100,
+        duration: 500,
+        itemWidth: double.infinity,
+        pagination: SwiperPagination(),
+        itemCount: 8,
+        itemBuilder: (BuildContext context, int index) => Image.asset(
+          'assets/images/banner1.jpg',
+          fit: BoxFit.cover,
+        ),
+        autoplay: true,
+        viewportFraction: 1.0,
+        scale: 0.9,
+      ),
+    );
+  }
+}
+
+class _IndianFoodView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => Column(
+            children: <Widget>[
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/food2.jpg',
+                  height: 50.0,
+                  width: 50.0,
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+        ));
+  }
 }
---
+++ lib/widgets/custom_divider_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomDividerView extends StatelessWidget {
+  final double dividerHeight;
+
+  CustomDividerView({
+    Key key,
+    this.dividerHeight = 10.0,
+  })  : assert(dividerHeight != 0.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: dividerHeight,
+      color: Colors.grey[200],
+    );
+  }
+}
